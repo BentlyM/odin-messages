9fb662e43aa4535d163631cc15366dc4cc4a7f2f
BentlyM <metayerbently@gmail.com>
2024-08-15 22:10:05 -0400

    added controller and ejs

--- src/controllers/messageControllers.ts
+++ src/controllers/messageControllers.ts
@@ -1,5 +1,23 @@
 
+const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date()
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date()
+    }
+  ];
+  
 
-export const homePage = (res : Request , req : any) => {
-    req.status(200).render('new', {message: 'hello world'});
+
+export const homePage = (res : Response , req : any) => {
+    req.status(200).render('index', {messages});
+}
+
+export const newMessage = (res : Response, req : any) => {
+  //req.status(200).render('new', )
 }
--- src/routes/messages.ts
+++ src/routes/messages.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-import { homePage } from '../controllers/messageControllers';
+import { homePage, newMessage } from '../controllers/messageControllers';
 
 const router = express.Router();
 
 router.get("/", homePage);
+
+router.get("/new", newMessage);
 
 export default router;
